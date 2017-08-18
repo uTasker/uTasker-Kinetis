6b3d24e937a925d84f4fa4e83ca969a9077ef160
uTasker <mark@uTasker.com>
2017-08-19 01:53:40 +0200

    MCUXpresso project added

---
+++ Applications/uTaskerV1.4/MCUXpresso/uTaskerV1.4_FLASH/generate.bat
@@ -0,0 +1,4 @@
+arm-none-eabi-objcopy  --output-target=binary uTaskerV1.4.elf ..\Applications\uTaskerV1.4\MCUXpresso\uTaskerV1.4_FLASH\uTaskerV1.4.bin
+arm-none-eabi-objcopy  --output-target=srec uTaskerV1.4.elf ..\Applications\uTaskerV1.4\MCUXpresso\uTaskerV1.4_FLASH\uTaskerV1.4.srec
+arm-none-eabi-objcopy  --output-target=ihex uTaskerV1.4.elf ..\Applications\uTaskerV1.4\MCUXpresso\uTaskerV1.4_FLASH\uTaskerV1.4.hex
+
